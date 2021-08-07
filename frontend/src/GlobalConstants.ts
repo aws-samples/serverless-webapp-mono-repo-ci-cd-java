05fe941a94a92f65e5c89205482fc8fed40c82b0
Pankaj Agrawal <pnkaj.agrwal@gmail.com>
2021-08-07 20:31:28 +0200

    Update GlobalConstants.ts

--- frontend/src/GlobalConstants.ts
+++ frontend/src/GlobalConstants.ts
@@ -18,7 +18,7 @@ GLOBAL_CONSTANTS.set('PYTHON', {
 export const Links_List = [
     {label: 'Source code for the project', link: 'https://github.com/aws-samples/serverless-webapp-mono-repo-ci-cd-java'},
     {label: 'Architecture diagram', link: '/serverless-webapp-mono-repo-ci-cd-java.png'},
-    {label: 'Amazon Rekognition', link: 'https://aws.aFormButtonsmazon.com/rekognition/'},
+    {label: 'Amazon Rekognition', link: 'https://aws.amazon.com/rekognition/'},
     {label: 'AWS Serverless Application Model', link: 'https://aws.amazon.com/serverless/sam/'},
     {label: 'AWS Cloud Development Kit (AWS CDK)', link: 'https://docs.aws.amazon.com/cdk/latest/guide/work-with-cdk-java.html'},
     {label: 'Learn more about AWS developer tools services?', link: 'https://aws.amazon.com/products/developer-tools/'},
